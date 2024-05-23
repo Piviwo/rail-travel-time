47a6b73e0fb2c3b60829029bdd44c4384f8988c8
Pia Wolffram <pia.wolffram@posteo.de>
2024-05-23 14:21:00 +0200

    changes

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -16,12 +16,29 @@ export const Navigation = () => {
           {isMenuOpen ? '✖' : '☰'}
         </div>
         <ul className="topNavList">
-          <li className="topNavItem"><a href="" className="topNavLink">Home</a></li>
-          <li className="topNavItem"><a href="" className="topNavLink">About</a></li>
-          <li className="topNavItem"><a href="" className="topNavLink">Contact</a></li>
+          <li className="topNavItem"><a href="" className="topNavLink">home</a></li>
+          <li className="topNavItem"><a href="" className="topNavLink">about</a></li>
+          <li className="topNavItem"><a href="" className="topNavLink">contact</a></li>
         </ul>
       </nav>
       <div className={`sideNavContainer ${isMenuOpen ? 'open' : ''}`}>
+        <div className="selectionContainer">
+          <h2>select your travel route</h2>
+          <div>
+            <p>from</p>
+            <select className="selectItem">
+              <option>city</option>
+              {/* to do: liste */}
+            </select>
+          </div>
+          <div>
+            <p>to</p>
+            <select className="selectItem">
+              <option>city</option>
+              {/* to do: liste */}
+            </select>
+          </div>
+        </div>
       </div>
     </div>
   );
