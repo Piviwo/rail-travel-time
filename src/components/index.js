8880006f25d408a3aa6e686616c5df9d1c7b6567
RicaThissen <rica@thissennet.de>
2024-06-06 11:03:13 +0200

    added sound and improved animation, one issue left: animation dependency on navbar on/off is not defiende yet

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { MapContainer } from "./map";
 export { Navigation, Header } from "./navigation";
 export { InfoContent } from "./infoContent";
+export { TrainAnimation } from "./trainAnimation";
