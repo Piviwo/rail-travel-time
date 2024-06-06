6f6406bf90d1d68023e5661da870a66c398b2062
Pia Wolffram <pia.wolffram@posteo.de>
2024-06-06 11:44:59 +0200

    add sidenav items

--- src/components/navigation/components/sideBar/sideBar.jsx
+++ src/components/navigation/components/sideBar/sideBar.jsx
@@ -4,30 +4,41 @@ import "./sideBar.css";
 export const SideBar = ({ isMenuOpen }) => {
   return (
     <div className={`sideNavContainer ${isMenuOpen ? "open" : ""}`}>
-      <div className="selectionContainer">
-        <h2>select your travel route</h2>
-        <div>
-          <p>mode</p>
-          {/* radio button  */}
-        </div>
-        <div>
-          <p>from</p>
-          <select className="selectItem">
-            <option>city</option>
-            {/* to do: liste */}
-          </select>
-        </div>
-        <div>
-          <p>to</p>
-          <select className="selectItem">
-            <option>city</option>
-            {/* to do: liste */}
-          </select>
-        </div>
-        {/* {two cities: average travel time} */}
-        {/*timetable within next hour: city, current time, gives trainnumber, final dest, highlight the cities */}
+    <div className="contentContainer">
+      <h2>select your travel route</h2>
+      <div className="radioContainer">
+        <label>
+          <input type="radio" value="average-times" name="radioGroup" defaultChecked/> average times
+        </label>
+        <label>
+          <input type="radio" value="timetable" name="radioGroup" /> time table
+        </label>
+      </div> 
+      <div className="selectContainer">
+        <p>from</p>
+        <select className="selectItem">
+          {/* to do: list */}
+          <option>city</option>
+          <option>city 1</option>
+          <option>city 2</option>
+          <option>city 3</option>
+          {/* to do: list */}
+        </select>
       </div>
+      <div className="selectContainer">
+        <p>to</p>
+        <select className="selectItem">
+          {/* to do: list */}
+          <option>city</option>
+          <option>city 1</option>
+          <option>city 2</option>
+          <option>city 3</option>
+        </select>
+      </div>
+      {/* {two cities: average travel time} */}
+      {/*timetable within next hour: city, current time, gives trainnumber, final dest, highlight the cities */}
     </div>
+  </div>
   );
 };
 
