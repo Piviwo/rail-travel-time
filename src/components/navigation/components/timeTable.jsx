eed2c8ee779bcde0c9e679a5faa2325e4ff4286a
RicaThissen <rica@thissennet.de>
2024-06-14 16:38:35 +0200

    added coordinated of destinations and selected Station

--- src/components/navigation/components/timeTable.jsx
+++ src/components/navigation/components/timeTable.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { getFinalData } from "./utils";
 import './timeTable.css';
+import citiesLocations from '../../../../data/cities_forTimetable.json'
 
 export const Timetable = ({ stationName }) => {
   //   const [evaNumber, setEvaNumber] = useState(null);
   const [timeTable, setTimeTable] = useState(null);
   const [selectedRow, setSelectedRow] = useState(null);
   const [coordinates, setCoordinates] = useState({ lat: null, lon: null });
+  const [coordinatesDest, setCoordinatesDest] = useState({ lat: null, lon: null });
   useEffect(() => {
     const fetchTimeTable = async () => {
       try {
@@ -19,6 +21,18 @@ export const Timetable = ({ stationName }) => {
     };
   
     fetchTimeTable();
+
+    const fetchStationCoordinates = async () => {
+      try {
+        const coordData = await fetchCoordinates(stationName);
+        setCoordinates({ lat: coordData.lat, lon: coordData.lon });
+        console.log(coordinates);
+      } catch (err) {
+        console.error('Failed to fetch coordinates', err);
+      }
+    };
+    fetchStationCoordinates();
+
   }, [stationName]);
 
   if (!timeTable) {
@@ -28,30 +42,29 @@ export const Timetable = ({ stationName }) => {
   const handleRowClick = async (index, entry) => {
     setSelectedRow(index);
     try {
-      const coordData = await fetchCoordinates(stationName);
-      setCoordinates({ lat: coordData.lat, lon: coordData.lon });
+      const coordData = await fetchCoordinates(entry.trainEnd);
+      setCoordinatesDest({ lat: coordData.lat, lon: coordData.lon });
+      console.log(coordinatesDest);
     } catch (err) {
       console.error('Failed to fetch coordinates', err);
     }
   };
   
   const fetchCoordinates = async (stationName) => {
-    const response = await fetch('/../../../../data/cities_for_Timetables.json');
-    console.log(response);
-    const data = await response.json();
-    // hier funktioniert es noch nicht!!
-  
-    const cityIndex = data.city.findIndex(cityName => cityName === stationName);
+    const stationNameLit = stationName.trim().replace(/\s+/g, '');
+    const cityDict = citiesLocations?.name;
+    const cityIndex = cityDict ? Object.entries(cityDict).find(([key, value]) => value.trim().replace(/\s+/g, '') === stationNameLit)?.[0]: undefined;
+    
     if (cityIndex !== -1) {
-      const lat = data.latitude[cityIndex];
-      const lon = data.longitude[cityIndex];
+      const lat = citiesLocations.latitude[cityIndex];
+      const lon = citiesLocations.longitude[cityIndex];
       return { lat, lon };
     } else {
       throw new Error('Station not found in data');
     }
   };
 
-  const departures = timeTable.filter(entry => entry.kind == 'departure' || entry.kind == 'intermediate_stop');
+  const departures = timeTable.filter(entry => entry.kind == 'departure' || entry.kind == 'intermediate_stop').sort((a, b) => new Date(`1970/01/01 ${a.departure}`) - new Date(`1970/01/01 ${b.departure}`));
   const arrivals = timeTable.filter(entry => entry.kind === 'arrival');
   
   return (
@@ -72,7 +85,7 @@ export const Timetable = ({ stationName }) => {
               <tr
               key={index}
               className={selectedRow === index ? 'selected' : ''}
-              onClick={() => handleRowClick(index)}>
+              onClick={() => handleRowClick(index,entry)}>
                 <td>{entry.departure}</td>
                 <td className="trainNumber">{entry.trainNumber}</td>
                 <td>{entry.trainEnd}</td>
